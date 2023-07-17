689977c2d850b44e8a91ff80d668461b9d9a4e05
Minyongggg <naminyong97@gmail.com>
2023-07-18 00:13:16 +0900

    [feat] Partner 엔티티 및 상속 엔티티 추가

---
+++ src/main/java/paibridge/apiheartee/partner/entity/Mbti.java
@@ -0,0 +1,8 @@
+package paibridge.apiheartee.partner.entity;
+
+public enum Mbti {
+    ENFP, ENFJ, ENTP, ENTJ,
+    ESFP, ESFJ, ESTP, ESTJ,
+    INFP, INFJ, INTP, INTJ,
+    ISFP, ISFJ, ISTP, ISTJ
+}
---
+++ src/main/java/paibridge/apiheartee/partner/entity/Partner.java
@@ -0,0 +1,55 @@
+package paibridge.apiheartee.partner.entity;
+
+import static javax.persistence.InheritanceType.JOINED;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import paibridge.apiheartee.common.entity.BaseEntity;
+import paibridge.apiheartee.member.entity.Member;
+
+@Entity
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Inheritance(strategy = JOINED)
+@DiscriminatorColumn(name = "dtype")
+public abstract class Partner extends BaseEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "partner_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private String nickname;
+    private String gender;
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private Mbti mbti;
+
+    public Partner(Member member, String nickname, String gender, Integer age, Mbti mbti) {
+        this.member = member;
+        this.nickname = nickname;
+        this.gender = gender;
+        this.age = age;
+        this.mbti = mbti;
+    }
+}
