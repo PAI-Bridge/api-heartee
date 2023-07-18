d90698cdb9be9b9885b155f97a03630fa51e8d7c
Minyongggg <naminyong97@gmail.com>
2023-07-18 16:17:42 +0900

    [fix] BodyDto, BodyListDto -> DataResponse, DataArrayResponse DTO명 변경

--- src/main/java/paibridge/apiheartee/common/dto/DataArrayResponse.java
+++ src/main/java/paibridge/apiheartee/common/dto/DataArrayResponse.java
@@ -2,14 +2,16 @@ package paibridge.apiheartee.common.dto;
 
 import java.util.List;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 // BodyDto를 상속해서 만들고 싶은데
 @Data
-public class BodyArrayDto<T> {
+@NoArgsConstructor
+public class DataArrayResponse<T> {
     private Integer count;
     private List<T> data;
 
-    public BodyArrayDto(List<T> data) {
+    public DataArrayResponse(List<T> data) {
         this.count = data.size();
         this.data = data;
     }
