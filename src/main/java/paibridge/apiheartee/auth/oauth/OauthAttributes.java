d93c3b0370bbaf912ad50872a4962101788bd09a
Minyongggg <naminyong97@gmail.com>
2023-08-25 00:28:32 +0900

    [feat] OAuth 인증 관련 handler, Service 레이어 추가

---
+++ src/main/java/paibridge/apiheartee/auth/oauth/OauthAttributes.java
@@ -0,0 +1,53 @@
+package paibridge.apiheartee.auth.oauth;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import paibridge.apiheartee.member.dto.MemberOauthDto;
+import paibridge.apiheartee.member.entity.OauthType;
+
+
+public enum OauthAttributes {
+    //    KAKAO("kakao", (attributes) -> {
+//        return new MemberOauthDto(
+//            OauthType.KAKAO,
+//            String.valueOf(attributes.get("sub")),
+//            (String) attributes.get("name"),
+//            (String) attributes.get("email"),
+//            (String) attributes.get("picture")
+//        );
+//    }),
+    GOOGLE("google", (attributes) -> {
+        return new MemberOauthDto(
+            OauthType.GOOGLE,
+            String.valueOf(attributes.get("sub")),
+            (String) attributes.get("name"),
+            (String) attributes.get("email")
+        );
+    }),
+    NAVER("naver", (attributes) -> {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+        return new MemberOauthDto(
+            OauthType.NAVER,
+            (String) response.get("id"),
+            (String) response.get("name"),
+            (String) response.get("email")
+        );
+    });
+
+    private final String registrationId;
+    private final Function<Map<String, Object>, MemberOauthDto> of;
+
+    OauthAttributes(String registrationId, Function<Map<String, Object>, MemberOauthDto> of) {
+        this.registrationId = registrationId;
+        this.of = of;
+    }
+
+    public static MemberOauthDto extract(String registrationId, Map<String, Object> attributes) {
+        return Arrays.stream(values())
+            .filter(provider -> registrationId.equals(provider.registrationId))
+            .findFirst()
+            .orElseThrow(IllegalArgumentException::new)
+            .of.apply(attributes);
+    }
+}
