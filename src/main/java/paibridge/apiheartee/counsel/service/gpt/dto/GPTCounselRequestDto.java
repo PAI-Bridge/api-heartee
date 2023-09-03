c9ecbaf7854127e302883b8ab53e1d014dfeff40
toto9602 <toto9602@naver.com>
2023-09-03 23:27:56 +0900

    [FIX] GPT에 요청을 보낼 때 사용할 필드명은 request body와 동일하도록 수정

--- src/main/java/paibridge/apiheartee/counsel/service/gpt/dto/GPTCounselRequestDto.java
+++ src/main/java/paibridge/apiheartee/counsel/service/gpt/dto/GPTCounselRequestDto.java
@@ -2,29 +2,33 @@ package paibridge.apiheartee.counsel.service.gpt.dto;
 
 import lombok.Builder;
 import lombok.Data;
+import paibridge.apiheartee.counsel.entity.CounselRequest;
 
 @Data
 public class GPTCounselRequestDto {
 
+    private CounselRequest counselRequest;
     private String language;
-    private String userGender;
-    private String counterpartGender;
-    private String conversationHistory;
+    private String user_gender;
+    private String counterpart_gender;
+    private String conversation_history;
 
     @Builder
-    public GPTCounselRequestDto(String language, String userGender, String counterpartGender, String conversationHistory) {
+    public GPTCounselRequestDto(CounselRequest counselRequest, String language, String user_gender, String counterpart_gender, String conversation_history) {
+        this.counselRequest = counselRequest;
         this.language = language;
-        this.userGender = userGender;
-        this.counterpartGender = counterpartGender;
-        this.conversationHistory = conversationHistory;
+        this.user_gender = user_gender;
+        this.counterpart_gender = counterpart_gender;
+        this.conversation_history = conversation_history;
     }
 
-    public static GPTCounselRequestDto create(String language, String userGender, String counterpartGender, String conversationHistory) {
+    public static GPTCounselRequestDto create(CounselRequest counselRequest, String language, String userGender, String counterpartGender, String conversationHistory) {
         return GPTCounselRequestDto.builder()
+                .counselRequest(counselRequest)
             .language(language)
-            .userGender(userGender)
-            .counterpartGender(counterpartGender)
-            .conversationHistory(conversationHistory)
+                .user_gender(userGender)
+            .counterpart_gender(counterpartGender)
+            .conversation_history(conversationHistory)
             .build();
     }
 }
