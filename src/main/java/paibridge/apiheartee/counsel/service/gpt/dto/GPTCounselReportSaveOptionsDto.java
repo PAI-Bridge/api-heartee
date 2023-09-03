bbb471d5b109513f17be2478f7719a52ef92b77c
toto9602 <toto9602@naver.com>
2023-09-03 23:27:15 +0900

    [FIX] report를 저장하기 위한 옵션으로 request의 ID가 필요

--- src/main/java/paibridge/apiheartee/counsel/service/gpt/dto/GPTCounselReportSaveOptionsDto.java
+++ src/main/java/paibridge/apiheartee/counsel/service/gpt/dto/GPTCounselReportSaveOptionsDto.java
@@ -2,18 +2,22 @@ package paibridge.apiheartee.counsel.service.gpt.dto;
 
 import lombok.Builder;
 import lombok.Data;
+import paibridge.apiheartee.counsel.entity.CounselRequest;
 
 @Data
 public class GPTCounselReportSaveOptionsDto {
     private String dType;
 
+    private CounselRequest counselRequest;
     @Builder
-    public GPTCounselReportSaveOptionsDto(String dType) {
+    public GPTCounselReportSaveOptionsDto(String dType, CounselRequest counselRequest) {
         this.dType = dType;
+        this.counselRequest = counselRequest;
     }
 
-    public static GPTCounselReportSaveOptionsDto create(String dType) {
+    public static GPTCounselReportSaveOptionsDto create(CounselRequest counselRequest, String dType) {
         return GPTCounselReportSaveOptionsDto.builder()
+                .counselRequest(counselRequest)
                 .dType(dType)
                 .build();
     }
