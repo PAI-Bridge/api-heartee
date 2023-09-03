c8e49c15c9cafc7b113faa25921a6dec272d99a3
toto9602 <toto9602@naver.com>
2023-09-03 23:11:33 +0900

    [FIX] 기존 statA, statB, 상속 entity별 stat을 프롬프트와 동일한 값으로 모두 통합

--- src/main/java/paibridge/apiheartee/counsel/dto/CounselReportDto.java
+++ src/main/java/paibridge/apiheartee/counsel/dto/CounselReportDto.java
@@ -11,50 +11,53 @@ import paibridge.apiheartee.counsel.entity.CounselReportGL;
 @Data
 public class CounselReportDto {
 
-    private Integer temperature;
-    private Integer statA;
-    private Integer statB;
-
-    private Integer statGL;
-    private Integer statDT;
-    private Integer statBU;
-
     private String summary;
     private String solution;
 
+    private Double willingness;
+    private Double selfOpenness;
+    private Double voiceOver;
+    private Double positiveLanguage;
+    private Double frequency;
+    private String explanation;
+
+
+
     @Builder
-    public CounselReportDto(Integer temperature, Integer statA, Integer statB, Integer statGL,
-        Integer statDT, Integer statBU, String summary, String solution) {
-        this.temperature = temperature;
-        this.statA = statA;
-        this.statB = statB;
-        this.statGL = statGL;
-        this.statDT = statDT;
-        this.statBU = statBU;
+    public CounselReportDto(String summary, String solution, Double willingness, Double selfOpenness, Double voiceOver, Double positiveLanguage, Double frequency, String explanation) {
         this.summary = summary;
         this.solution = solution;
+        this.willingness = willingness;
+        this.selfOpenness = selfOpenness;
+        this.voiceOver = voiceOver;
+        this.positiveLanguage = positiveLanguage;
+        this.frequency = frequency;
+        this.explanation = explanation;
     }
 
     public static CounselReportDto create(CounselReport entity) {
         CounselReportDto dto = CounselReportDto.builder()
-            .temperature(entity.getTemperature())
-            .statA(entity.getStatA())
-            .statB(entity.getStatB())
             .summary(entity.getSummary())
             .solution(entity.getSolution())
+                .willingness(entity.getWillingness())
+                .selfOpenness(entity.getSelfOpenness())
+                .voiceOver(entity.getVoiceOver())
+                .positiveLanguage(entity.getPositiveLanguage())
+                .frequency(entity.getFrequency())
+                .explanation(entity.getExplanation())
             .build();
-
-        if (entity.getDtype().equals(Values.GL)) {
-            dto.setStatGL(((CounselReportGL) entity).getStatGL());
-        }
-
-        if (entity.getDtype().equals(Values.DT)) {
-            dto.setStatDT(((CounselReportDT) entity).getStatDT());
-        }
-
-        if (entity.getDtype().equals(Values.BU)) {
-            dto.setStatBU(((CounselReportBU) entity).getStatBU());
-        }
+//
+//        if (entity.getDtype().equals(Values.GL)) {
+//            dto.setStatGL(((CounselReportGL) entity).getStatGL());
+//        }
+//
+//        if (entity.getDtype().equals(Values.DT)) {
+//            dto.setStatDT(((CounselReportDT) entity).getStatDT());
+//        }
+//
+//        if (entity.getDtype().equals(Values.BU)) {
+//            dto.setStatBU(((CounselReportBU) entity).getStatBU());
+//        }
 
         return dto;
     }
